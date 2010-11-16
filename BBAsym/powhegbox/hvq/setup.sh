5408117ce8a6463940ca8eaafb919285527d5c69
aran <>
2010-11-16 16:38:34 +0000

    POWHEG-BOX files I hacked to run bb generation with an effective invariant mass cut.

---
+++ BBAsym/powhegbox/hvq/setup.sh
@@ -0,0 +1,5 @@
+# Follow instructions here: https://twiki.cern.ch/twiki/bin/view/Main/PowhegBOX#Recipe_for_dummies
+export LHAPDFSYS=/afs/cern.ch/user/a/aran/scratch0/powheg/LHAPDF5.8.4
+export PATH=${PATH}:${LHAPDFSYS}/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LHAPDFSYS}/lib 
+export PATH=${PATH}:/afs/cern.ch/user/a/aran/scratch0/powheg/FASTJET2.4.2/bin/ 
